e17bf5630771d0a347d9952989913e53a8034acb
Fernando-TB <f.torres17@ufromail.cl>
2025-08-29 08:55:25 -0400

    1

---
+++ src/main/java/UtilidadesTexto.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class UtilidadesTexto {
+    public static void main(){
+
+    }
+
+    public static void menu(){
+        mostrarMenu();
+    }
+    public static void mostrarMenu(){
+        System.out.println("1.");
+        System.out.println("2.");
+        System.out.println("3.");
+        System.out.println("4.");
+        System.out.println("5.");
+        System.out.println("Indique su seleccion: ");
+    }
+
+    public static int leerOpcion(){
+        Scanner opcion = new Scanner();
+    }
+
+}
