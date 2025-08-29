5c30f3be5f8f186aa470d22eedb0061c6599b842
Fernando-TB <f.torres17@ufromail.cl>
2025-08-29 09:30:53 -0400

    Inicio primer metodo

--- src/main/java/UtilidadesTexto.java
+++ src/main/java/UtilidadesTexto.java
@@ -7,18 +7,40 @@ public class UtilidadesTexto {
 
     public static void menu(){
         mostrarMenu();
+        Scanner opcion = new Scanner(System.in);
+        int seleccion = leerOpcion(opcion);
+        ejecutarOpcion(seleccion,opcion);
+
     }
     public static void mostrarMenu(){
-        System.out.println("1.");
-        System.out.println("2.");
-        System.out.println("3.");
-        System.out.println("4.");
-        System.out.println("5.");
+        System.out.println("1. Verificar si frase es Reves-Derecho");
+        System.out.println("2.Contar vocales de una frase");
+        System.out.println("3.Encriptar frase");
+        System.out.println("4.Desencriptar una frase");
+        System.out.println("5.Salir");
         System.out.println("Indique su seleccion: ");
     }
 
-    public static int leerOpcion(){
-        Scanner opcion = new Scanner(System.in);
+    public static int leerOpcion(Scanner opcion){;
+        return opcion.nextInt();
     }
+
+    public static void ejecutarOpcion(int opcion,Scanner lector){
+        switch (opcion) {
+            case 1 -> solicitarFrase(lector);
+        }
+    }
+
+    public static String solicitarFrase(Scanner frase){
+        System.out.println("Indique su frase: ");
+        return frase.nextLine();
+    }
+
+    public static void verificarPalindromo(Scanner leer){
+        String frase = solicitarFrase(leer);
+
+
+    }
+
 
 }
